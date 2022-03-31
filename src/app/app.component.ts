cd6a5821c03c4805bd41f5e38225036ab5d7d0bd
Edson Junior <edsonjos61@gmail.com>
2022-03-31 09:47:50 -0300

    Working with modules and components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angula-Course';
+  title = 'Angular-Course';
+
+  name : string = 'Edson Junior';
 }
